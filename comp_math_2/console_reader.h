af4be605077ac7c5e15d145cccd9cfc255551eaf
slayer404 <doomer888@protonmail.com>
2021-04-07 00:36:45 +0300

    fix

---
+++ comp_math_2/console_reader.h
@@ -0,0 +1,16 @@
+#ifndef _CONSOLE_READER_H_
+#define _CONSOLE_READER_H_
+
+#include <iostream>
+
+class ConsoleReader
+{
+
+public:
+	void readInput(double* a, double* b, double* x, double* eps);
+
+};
+
+#endif
+
+
---
+++ comp_math_2/file_reader.h
@@ -0,0 +1,17 @@
+#ifndef _FILE_READER_H_
+#define _FILE_READER_H_
+
+#include <fstream>
+#include <iostream>
+
+class FileReader
+{
+
+public:
+	bool readInput(const char* path, double* a, double* b, double* x, double* eps);
+
+};
+
+#endif
+
+
